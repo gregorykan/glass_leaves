96615f0aef76f2d29b34189b42ca92be7676254e
Gregory Kan <kan.gregory@gmail.com>
2016-01-21 14:41:23 +1300

    organised array

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -31,7 +31,17 @@ class HomeController < ApplicationController
   end
 
   def procedures
-    [['Burn', 1], ['Randomize', 2], ['Get Nouns', 3], ['Get Nouns and Adjectives', 4], ['Get Adjectives', 5], ['Get Verbs', 6], ['Remove Nouns', 7], ['Remove Adjectives', 8], ['Remove Verbs', 9], ['Splice Words', 10], ['Splice Sentences', 11]]
+    [['Burn', 1],
+    ['Randomize', 2],
+    ['Get Nouns', 3],
+    ['Get Nouns and Adjectives', 4],
+    ['Get Adjectives', 5],
+    ['Get Verbs', 6],
+    ['Remove Nouns', 7],
+    ['Remove Adjectives', 8],
+    ['Remove Verbs', 9],
+    ['Splice Words', 10],
+    ['Splice Sentences', 11]]
   end
 
 end
