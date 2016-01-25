9205230a6ab45f3bdd4eaa102c81b1821f280d0f
Gregory Kan <kan.gregory@gmail.com>
2016-01-26 10:28:47 +1300

    retaining punctuation in noun and adjective isolator. using select

--- app/services/manipulators/noun_and_adjective_isolator.rb
+++ app/services/manipulators/noun_and_adjective_isolator.rb
@@ -5,7 +5,10 @@ module Manipulators
     end
 
     def isolate
-      get_nouns_and_adjectives.join(" ")
+      word_array = split
+      nouns_and_adjectives_array = get_nouns_and_adjectives
+      selected_words = word_array.select { |x| nouns_and_adjectives_array.include? x }
+      selected_words.join(" ")
     end
 
     def get_nouns_and_adjectives
@@ -13,6 +16,12 @@ module Manipulators
       nouns = eng_tagger.get_nouns(tagged_string).map { |k, v| k }
       adjectives = eng_tagger.get_adjectives(tagged_string).map { |k, v| k }
       nouns + adjectives
+    end
+
+    def split
+      aerated = eng_tagger.get_sentences(@string)
+      joined = aerated.join(" ")
+      word_array = joined.split(" ")
     end
 
     def eng_tagger
