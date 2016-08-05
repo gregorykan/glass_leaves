7204671ee993dc8dfba1013d9d5fa37f0256fe96
Gregory Kan <kan.gregory@gmail.com>
2016-08-05 12:58:42 +1200

    added class stats method

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,9 @@
 class Event < ActiveRecord::Base
+  def self.stats
+    self.all.map do |event|
+      time = event.created_at.in_time_zone("Auckland")
+      procedure = event.comment
+      puts "#{procedure} at #{time}"
+    end
+  end
 end
